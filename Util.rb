b77e72122a94cb2799f9d1385f4b6f675a2c3ce6
Brian Jacobs <bxj9527@gmail.com>
2017-01-06 18:52:22 -0500

    added the run file and a constants file

---
+++ Util.rb
@@ -0,0 +1,8 @@
+FILENAMES = [
+"GoFish",
+"52CardPickup"
+]
+GAMELISTING = {
+    "52 Card Pickup" => "CardPickup"
+}
+WELCOME = "Welcome"
--- runTable.rb
+++ runTable.rb
@@ -1,6 +1,20 @@
-require_relative 'Games/Table'
-
-table = Table.new
-(0..51).each do |i|
-    puts table.next
+require_relative 'Util'
+FILENAMES.each do |game|
+    require_relative 'Games/' + game
 end
+
+def CardPickup()
+    table = CardPickup.new
+    puts table
+    (0..51).each do
+        puts table.next
+    end
+end
+
+def main()
+    puts WELCOME
+    game = gets
+    send(GAMELISTING["52 Card Pickup"])
+end
+
+main
