e73276b5bc116fabb6f89b131f5171bf27841fb1
Brian Jacobs <bxj9527@gmail.com>
2017-01-06 16:51:38 -0500

    Implemented methods; push, pop, and index for LinkedList

--- LinkedList.rb
+++ LinkedList.rb
@@ -4,10 +4,14 @@ class LinkedList
 
     @head
     @tail
+    @length
+
+    attr_reader :length
 
     def initialize()
         @head = nil
         @tail = nil
+        @length = 0
     end
 
     def push(value)
@@ -19,12 +23,25 @@ class LinkedList
             @tail.next = node
             @tail = node
         end
+        @length += 1
     end
 
     def pop()
+        return nil if length == 0
+        node = @head
+        @head = @head.next
+        @length -= 1
+        return node.value
     end
 
-    def index()
+    def index(value)
+        return nil if value > @length
+        node = @head
+        while value >= 0 do
+            return node.value if value == 0
+            value -= 1
+            node = node.next
+        end
     end
 
 end
